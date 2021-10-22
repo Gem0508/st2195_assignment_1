fdd7c8520dfa96d5e42405860f5a2152a048bae0
Gem <lh83830508@gmail.com>
2021-10-23 00:27:19 +0800

    Assignment 1 Review 1

---
+++ readme.md
@@ -0,0 +1,5 @@
+This is a readme file.
+
+This is also the First Question of Practice Assignment 1
+
+Hello_World
---
+++ spyder/untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 00:13:59 2021
+
+@author: User
+"""
+
+#%%
+for i in range(1,11):
+    print(i)
+    
+    
+#%%
+
